a958f868dea8dd0b244ef5aed87948402c397c04
thiagomeloo <4thiagomelo5@gmail.com>
2019-11-06 15:37:33 -0300

    add usuario nos itens do menu.

--- app/View/cabecalho.php
+++ app/View/cabecalho.php
@@ -17,9 +17,16 @@
         <div class="container-fluid d-flex flex-column p-0">
             <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="/main">
                 <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
-                <div class="sidebar-brand-text mx-3"><span>Brand</span></div>
+                <div class="sidebar-brand-text mx-3"><span>S W E</span></div>
             </a>
             <hr class="sidebar-divider my-0">
+            <ul class="nav navbar-nav text-light">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" href=""><i class=""></i>
+                        <img class="border rounded-circle img-profile mx-auto" src="assets/img/user-icon.jpg" />
+                        <span class="p-1 my_FontColor">NOME USUARIO</span>
+                    </a></li>
+            </ul>
             <?php
             foreach ($itens as $key => $value){ ?>
             <ul class="nav navbar-nav text-light">

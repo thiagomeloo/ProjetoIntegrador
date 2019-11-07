b5f550fcb093045099b811d774ef20ab29d95711
thiagomeloo <4thiagomelo5@gmail.com>
2019-11-07 15:52:57 -0300

    criação do form de cadastro de adm

---
+++ app/View/form-cadastrar-admin.php
@@ -0,0 +1,67 @@
+<div class="container-fluid">
+    <form>
+        <hr>
+        <h5 class="font-weight-bold">Dados Pessoais</h5>
+        <hr>
+        <div class="form-group">
+
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label>Nome</label>
+                <input type="text" class="form-control" placeholder="Nome Completo">
+            </div>
+            <div class="form-group col-md-6">
+                <label>CPF</label>
+                <input type="text" class="form-control" placeholder="CPF">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <label>Data de Nascimento</label>
+                <input type="date" class="form-control" placeholder="Data de Nascimento">
+            </div>
+            <div class="form-group col-md-5">
+                <label>Email</label>
+                <input type="email" class="form-control" placeholder="Email">
+            </div>
+            <div class="form-group col-md-4">
+                <label>Senha</label>
+                <input type="password" class="form-control" placeholder="Senha">
+            </div>
+        </div>
+
+        <hr>
+        <h5 class="font-weight-bold">Endereço</h5>
+        <hr>
+        <div class="form-row">
+            <div class="form-group col-md-5">
+                <label>Rua</label>
+                <input type="text" class="form-control" placeholder="Rua">
+            </div>
+            <div class="form-group col-md-2">
+                <label>Número</label>
+                <input type="number" class="form-control" placeholder="Número">
+            </div>
+            <div class="form-group col-md-5">
+                <label>Bairro</label>
+                <input type="text" class="form-control" placeholder="Bairro">
+            </div>
+            <div class="form-group col-md-4">
+                <label>Cidade</label>
+                <input type="text" class="form-control" placeholder="Cidade">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputPassword4">Estado</label>
+                <input type="password" class="form-control" placeholder="Estado">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputPassword4">CEP</label>
+                <input type="password" class="form-control" placeholder="CEP">
+            </div>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>

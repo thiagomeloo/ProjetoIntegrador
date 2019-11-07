69741a62866c38f43f9567d512dfdfa754ad2102
thiagomeloo <4thiagomelo5@gmail.com>
2019-11-07 15:54:23 -0300

    Limpeza de arquivos

--- app/View/contentGerenciarAdmin.php
+++
@@ -1,5 +0,0 @@
- <div class="container-fluid">
-        <div class="d-sm-flex justify-content-between align-items-center mb-4">
-            <h3 class="text-dark mb-0">Gerenciar Administrador</h3>
-        </div>
- </div>

18d67a9cec2713dbc0d334ee8843b337dc46427b
thiagomeloo <4thiagomelo5@gmail.com>
2019-11-08 10:31:51 -0300

    inserção de campos do usuario

--- app/src/Entity/Usuario.php
+++ app/src/Entity/Usuario.php
@@ -9,8 +9,13 @@ use Ifnc\Tads\Helper\Record;
 class Usuario extends Record
 {
     public $id;
+    public $nome;
+    public $data_de_nascimento;
+    public $cpf;
     public $email;
     public $senha;
+    public $tipo;
+    public $status;
 
     public function valide($senha){
         return password_verify($senha,$this->senha);

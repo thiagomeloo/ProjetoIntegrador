f64a33578797687f5fb4944ae5d3994fca5d6ce7
thiagomeloo <4thiagomelo5@gmail.com>
2019-11-09 01:09:22 -0300

    add classe para confirmar validação de email

--- app/src/Controller/ValidaUserController.php
+++ app/src/Controller/ValidaUserController.php
@@ -1,33 +1,27 @@
 <?php
 
-
 namespace Ifnc\Tads\Controller;
-
 
 use Ifnc\Tads\Entity\Usuario;
 use Ifnc\Tads\Helper\Transaction;
 
 class ValidaUserController implements IController
-
 {
     public function request(): void
     {
         Transaction::open();
-        $usuario = Usuario::find($_GET['id']);
+        $usuario = Usuario::findByCondition("email='{$_POST['email']}'");
+        if (is_null($usuario) || !$usuario->valide($_POST['codigo'])) {
+            //header('Location: /login-form');
+            exit();
+        }
+        $usuario->status = 1;
+        $usuario->senha = password_hash($_POST['senha'], PASSWORD_ARGON2I);
+        $usuario->store();
         Transaction::close();
 
-       if($usuario != null){
-           $usuario->status = 1;
-           Transaction::open();
-           $usuario->store();
-           Transaction::close();
-           header("Location: /main", true, 302);
-
-           exit();
-       }
-
-
-
+        //header("Location: /login-form", true, 302);
+        exit();
 
     }
 }

9ed14c83ef44edb3e17566969cad115e6f720348
thiagomeloo <4thiagomelo5@gmail.com>
2019-11-09 01:08:47 -0300

    add classe para manipulação e envio de email

--- app/src/Helper/Send_Email.php
+++ app/src/Helper/Send_Email.php
@@ -5,8 +5,10 @@ require 'vendor/autoload.php';
 
 class Send_Email
 {
-
-
+    static function random_key($chars = 8) {
+        $letters = 'abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        return substr(str_shuffle($letters), 0, $chars);
+    }
 
 }
 
